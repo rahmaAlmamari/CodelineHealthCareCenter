1e36edf4d9c0c226405bc8b7b56532afc1112b64
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-07-27 15:10:56 +0400

    Moved All classes to Models folder

--- Models/Clinic.cs
+++ Models/Clinic.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodelineHealthCareCenter
+namespace CodelineHealthCareCenter.Models
 {
-    class Doctor
+    class Clinic
     {
     }
 }
