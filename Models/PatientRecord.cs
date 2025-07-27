bb9876ad5af66b3a23c4eb4fc56c7a67cd35727f
rahmaAlmamari <alr816125@gmail.com>
2025-07-27 16:00:10 +0400

    Added PatientRecord Fields

--- Models/PatientRecord.cs
+++ Models/PatientRecord.cs
@@ -9,6 +9,12 @@ namespace CodelineHealthCareCenter.Models
     class PatientRecord
     {
         //1. class feilds ...
+        public int PatientRecordId;
+
+        public List<string> Services = new List<string>();
+        public double TotalCost;
+        public string DoctorNote;
+        public static int PatientRecordCount = 0;
 
         //====================================================
         //2. class properity ...
