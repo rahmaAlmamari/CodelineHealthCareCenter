1c99fdb812f9c34dd305332f30a1d475dfbead34
rahmaAlmamari <alr816125@gmail.com>
2025-07-27 16:23:09 +0400

    Add btanch fields

--- Models/Branch.cs
+++ Models/Branch.cs
@@ -8,7 +8,15 @@ namespace CodelineHealthCareCenter.Models
 {
     class Branch
     {
-        //1. class feilds ...
+        //1. class fields ...
+        public int BranchId;
+        public string BranchName;
+        public string BranchCity;
+        public DateOnly BranchEstablishDate;
+        public bool BranchStatus = true; // true means open, false means closed
+        public static int BranchCount = 0;
+        public List<Floor> Floors = new List<Floor>();
+        public int HospitalId;
 
         //====================================================
         //2. class properity ...
