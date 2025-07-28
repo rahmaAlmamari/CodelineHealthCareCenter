e00e8c747472d25a80c887fd30ebd1ea639e55fa
Amani Khalid <amanikhalidd@gmail.com>
2025-07-28 09:43:21 +0400

    Modified Clinic fields

--- Models/Clinic.cs
+++ Models/Clinic.cs
@@ -9,18 +9,17 @@ namespace CodelineHealthCareCenter.Models
     class Clinic
     {
         //1. class fields ...
-        public int ClinicId;
-        public string ClinicName;
-        public int DepartmentId;
+      
         bool ClinicStatus = true; // true means open, false means closed
-        public int FloorId;
-        public int RoomId;
-        public List<Doctor> Doctors = new List<Doctor>();
-        public List<DateTime> ClinicSpote = new List<DateTime>();
         public static int ClinicCount = 0;
 
         //====================================================
         //2. class properity ...
+        
+       
+
+
+
 
         //====================================================
         //3. class method ...
