bcc7414d5f8066075732497a3baa6467546852d4
rahmaAlmamari <alr816125@gmail.com>
2025-07-28 08:32:47 +0400

    Adding method to IBookingService interface

--- Services/IBookingService.cs
+++ Services/IBookingService.cs
@@ -8,5 +8,16 @@ namespace CodelineHealthCareCenter.Services
 {
     interface IBookingService
     {
+        void BookAppointment();
+        void CancelAppointment();
+        void DeleteAppointment();
+        void UpdateBookedAppointment();
+        void GetAllBooking();
+        void GetBookingById(int id);
+        void GetBookingByClinicIdAndDate(int patientId);
+        void GetBookingByPatientId(int patientId);
+        void GetAvailableAppointmentsByClinicIdAndDate(DateTime date, int clinicId);
+        void ScheduleAppointment(int patientId, int clinicId, DateTime date, TimeSpan time);
+
     }
 }
