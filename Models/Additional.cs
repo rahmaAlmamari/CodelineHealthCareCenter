24d01b6cdd0a6f57ce301d0c9a0c2c56ee41dd3b
rahmaAlmamari <alr816125@gmail.com>
2025-07-28 07:48:11 +0400

    Adding HoldScreen method in Additional class

--- Models/Additional.cs
+++ Models/Additional.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodelineHealthCareCenter.Models
 {
     class Additional
     {
+        //1. To hoad the screen ...
+        public static void HoldScreen()
+        {
+            Console.WriteLine("Press (Enter Kay) to continue");
+            Console.ReadLine();
+        }
+
     }
 }
