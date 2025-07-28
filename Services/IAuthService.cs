862c6a01f7ead5b6f91926af64f27bbb6527630e
rahmaAlmamari <alr816125@gmail.com>
2025-07-28 10:57:33 +0400

    Update IAuthService and IUserService

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -8,7 +8,8 @@ namespace CodelineHealthCareCenter.Services
 {
     interface IAuthService
     {
-        void Login(string nationalId, string password);
+        //void Login(string nationalId, string password);
+        //public void AddPatient(string username, string password, string email, int phoneNumber, string userNationalID);//to sinUp new patient ...
 
 
     }
--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -10,17 +10,17 @@ namespace CodelineHealthCareCenter.Services
     interface IUserService
     {
         // Methods for User Service ...
-        void AddSuperAdmin(string username, string password, string email);
-        User AuthenticteUser(string username, string password);
-        void DeactiveUser(int userId);
-        bool EmailExists(string email);
-        User GetUserById(int userId);
-        User GetUserByName(string username);
-        void GetUserName(int userId);
-        void UpdatePassword(int userId, string newPassword);
-        void UpdateUser(int userId, string username, string email, string password, bool isActive);
-        void GetUserData(int userId);
-        void GetUserByRole(string roleName);
-        void DeleteUser(int userId);
+        //void AddSuperAdmin(string username, string password, string email);
+        //User AuthenticteUser(string username, string password);
+        //void DeactiveUser(int userId);
+        //bool EmailExists(string email);
+        //User GetUserById(int userId);
+        //User GetUserByName(string username);
+        //void GetUserName(int userId);
+        //void UpdatePassword(int userId, string newPassword);
+        //void UpdateUser(int userId, string username, string email, string password, bool isActive);
+        //void GetUserData(int userId);
+        //void GetUserByRole(string roleName);
+        //void DeleteUser(int userId);
     }
 }
