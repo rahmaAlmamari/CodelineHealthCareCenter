731281555d39741752ab670526cf00f579993796
Amani Khalid <amanikhalidd@gmail.com>
2025-07-28 08:13:22 +0400

    Added Methods for Branch Department Service

--- Services/IBranchDepartmentService.cs
+++ Services/IBranchDepartmentService.cs
@@ -8,5 +8,12 @@ namespace CodelineHealthCareCenter.Services
 {
     interface IBranchDepartmentService
     {
+        // Methods for Branch Department Service ...
+        void AddDepartmentToBranch(int branchId, int departmentId);
+        void GetDepartmentsByBranch(int branchId);
+        void GetBranchesByDepartment(int departmentId);
+        void UpdateBranchDepartment(int branchId, int departmentId, string newDepartmentName);
+        void GetBranchDep(int branchId, int departmentId);
+        void GetDepartmentByBranchName(string BranchName);
     }
 }
