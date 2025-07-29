61270991177f0f758364b45aa41afe5cbe4855d7
Amani Khalid <amanikhalidd@gmail.com>
2025-07-29 11:41:42 +0400

    Created IHasService Interface

---
+++ Services/IHasService.cs
@@ -0,0 +1,14 @@
+﻿using CodelineHealthCareCenter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodelineHealthCareCenter.Services
+{
+    interface IHasService
+    {
+        List<Service> Services { get; set; }
+    }
+}
