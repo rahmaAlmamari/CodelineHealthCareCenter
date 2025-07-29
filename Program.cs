84606945b498fa860491070c29db065072c23100
rahmaAlmamari <alr816125@gmail.com>
2025-07-29 10:23:03 +0400

    update program

--- Program.cs
+++ Program.cs
@@ -1,14 +1,46 @@
 ﻿using CodelineHealthCareCenter.Models;
+using System.Diagnostics;
 
 namespace CodelineHealthCareCenter
 {
     
 internal class Program
     {
-        public static Branch testBranch = new Branch("TestBranch", "Muscat", new DateOnly(2025, 7, 28), 1); // Create a test branch object
+        // to create a test branch for the hospital
+        public static Branch testBranch = new Branch("TestBranch", "Muscat", new DateOnly(2025, 7, 28), 1);
+        // to create a test floor for the branch
+        public static Floor testFloor = new Floor();
+        //to create a test department for the branch
+        public static Department DepartmentTest = new Department();
+        //to create a test clinic for the department
+        public static Clinic ClinicTest = new Clinic("head", "muscat", 1, 1, 1, 1, 20);
+        //to create a test patient for the branch
+        public static Patient PatientTest = new Patient();
+        // to create a test doctor for the branch
+        public static Doctor DoctorTest = new Doctor("Ali", "Ali@gmail.com", "Head", 1, 1);
+
         static void Main(string[] args)
         {
-            Hospital.Branches.Add(testBranch); // Add the test branch to the hospital branches
+            // to add the test branch to the hospital branches
+            Hospital.Branches.Add(testBranch);
+            // to add the test floor to the branch floors
+            testBranch.Floors.Add(testFloor);
+            //to add the test department to the branch departments
+            DepartmentTest.BranchId = 1;
+            DepartmentTest.DepartmentName = "Cardiology";
+            BranchDepartment.Departments.Add(DepartmentTest);
+            //to add the test clinic to the department clinics
+            DepartmentTest.Clinics.Add(ClinicTest);
+            //to add the test patient to the branch patients
+            PatientTest.UserName = "Rahma";
+            PatientTest.P_UserPassword = "123";
+            PatientTest.UserNationalID = "11rr22";
+            PatientTest.UserEmail = "rahma@gmail.com";
+            PatientTest.PatientCity = "Muscat";
+            PatientTest.P_UserPhoneNumber = 12345678;
+            testBranch.Patients.Add(PatientTest);
+            //to add the test doctor to the branch doctors
+            BranchDepartment.Doctors.Add(DoctorTest);
             //to display the welcome message ...
             Additional.WelcomeMessage("Hospital Management");
             User x = new User();
